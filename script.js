2cbe77a4e1b553f5643ddda098fa524429daf9c2
AYDJI <158556316+AYDJI@users.noreply.github.com>
2024-11-14 09:32:41 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        alert("ברוכים הבאים לאתר לזכר מלחמת יום הכיפורים");
-    }, 1000);  // Delayed for a more gentle welcome
-});
