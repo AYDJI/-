778593e999c0dc4f123c51a708942d8729bf478f
AYDJI <158556316+AYDJI@users.noreply.github.com>
2024-11-14 09:33:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        alert("ברוכים הבאים לאתר לזכר מלחמת יום הכיפורים");
+    }, 1000);  // Delayed for a more gentle welcome
+});
